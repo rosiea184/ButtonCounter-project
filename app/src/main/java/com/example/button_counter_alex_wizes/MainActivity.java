3f7580e9a91e9ec3fdf9672f463db6adb1f91a27
Alex <rosiea184@gmail.com>
2021-02-09 17:59:27 -0500

    new buttons

--- app/src/main/java/com/example/button_counter_alex_wizes/MainActivity.java
+++ app/src/main/java/com/example/button_counter_alex_wizes/MainActivity.java
@@ -8,12 +8,19 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG="MainActivity";
+    private Button clear;
+    private Button increase;
+    private Button decrease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +29,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        clear = findViewById(R.id.button2);
+        clear.setText("Clear");
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                        Log.i(TAG, "I pressed clear");
+            }
+
+        });
+        increase = findViewById(R.id.button_first);
+        increase.setText("Plus 1");
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                Log.i(TAG, "I pressed increase");
+            }
+        });
+        decrease = findViewById(R.id.button_second);
+        decrease.setText("Minus 1");
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                Log.i(TAG, "I pressed decrease");
             }
         });
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "I entered into onStart()");
+    }
+
+
+    /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -52,5 +86,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
