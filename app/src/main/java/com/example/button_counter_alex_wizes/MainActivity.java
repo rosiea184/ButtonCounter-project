bfb68f1ae2d20c7bd83e937997896f2e53672a7a
Alex <rosiea184@gmail.com>
2021-02-10 09:14:22 -0500

    buttons

--- app/src/main/java/com/example/button_counter_alex_wizes/MainActivity.java
+++ app/src/main/java/com/example/button_counter_alex_wizes/MainActivity.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,42 +24,64 @@ public class MainActivity extends AppCompatActivity {
     private Button clear;
     private Button increase;
     private Button decrease;
+    private TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        display = findViewById(R.id.displayId);
 
         clear = findViewById(R.id.button2);
-        clear.setText("Clear");
+        //clear.setText("Clear");
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                //Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
                         Log.i(TAG, "I pressed clear");
+                        resetCounter();
             }
 
         });
-        increase = findViewById(R.id.button_first);
-        increase.setText("Plus 1");
+        increase = findViewById(R.id.incButtonId);
+        //increase.setText("Plus 1");
         increase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                //Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
                 Log.i(TAG, "I pressed increase");
+                //count here
+                incrementCounter();
             }
         });
-        decrease = findViewById(R.id.button_second);
-        decrease.setText("Minus 1");
+        decrease = findViewById(R.id.decButtonId);
+        //decrease.setText("Minus 1");
         decrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+                //Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
                 Log.i(TAG, "I pressed decrease");
+                decrementCounter();
             }
         });
+    }
+
+    private void incrementCounter(){
+        int currentValue = Integer.valueOf(display.getText().toString());
+        currentValue++;
+        display.setText(String.valueOf(currentValue));
+    }
+
+    private void decrementCounter(){
+        int currentValue = Integer.valueOf(display.getText().toString());
+        currentValue--;
+        display.setText(String.valueOf(currentValue));
+    }
+    private void resetCounter(){
+        int currentValue = Integer.valueOf(display.getText().toString());
+        currentValue = 0;
+        display.setText(String.valueOf(currentValue));
     }
 
     @Override
@@ -65,26 +90,18 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "I entered into onStart()");
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-    /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        String counterValue = display;
+        outState.putString("display", counterValue);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String counterValue = savedInstanceState.getString("display");
+        display.setText(counterValue);
+    }
 }
